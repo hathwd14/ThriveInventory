c8a325177aedc8939a3e6c81e29c00523b79cd94
Victor Geary <vgeary@Victors-MBP.dhcp.wfu.edu>
2018-04-01 20:03:25 -0400

    Added files

---
+++ routes/index.js
@@ -0,0 +1,13 @@
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', sam);
+
+function sam(req, res, next) {
+	res.render('index',
+		{title: "Best Movie Characters of all Time"});
+}
+
+module.exports = router;
