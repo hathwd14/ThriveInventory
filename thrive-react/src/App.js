e0cb5ccfd7d80dd6d10e7b37200dc0f8e7f5ee69
Victor Geary <vgeary@Victors-MBP.dhcp.wfu.edu>
2018-04-04 13:49:52 -0400

    Added table skeleton

---
+++ thrive-react/src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import BasicTable from './basic-table.jsx';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <BasicTable />
+      </div>
+    );
+  }
+}
+
+export default App;
